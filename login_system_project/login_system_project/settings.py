96b8fcb683bfbb13a41ff5723972a6e001f5ad93
Ankur-Kaul-24 <86168411+AnkurKaul24@users.noreply.github.com>
2021-09-29 16:49:46 +0530

    Update settings.py

--- login_system_project/login_system_project/settings.py
+++ login_system_project/login_system_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*94)jf48h(cehk(g2l%bmt&&@da-y5tpp(kou%%k7u!d&#^^bx'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
